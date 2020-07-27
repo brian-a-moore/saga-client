b7106e2dd6935bd755cd7ac6bd4a727d0d03737c
Brian Moore <brianmoore0213@gmail.com>
2020-07-26 21:39:02 -0400

    changes

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Editor from '../playground/ckeditor';
 
 const App = () => {
     return(
         <div>
-            <Editor />
+            My App
         </div>
     );
 }
