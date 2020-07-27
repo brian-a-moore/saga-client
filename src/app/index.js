46b688dbbcff71bb8974c648ec1da5984e8266f1
Brian Moore <brianmoore0213@gmail.com>
2020-07-26 21:10:09 -0400

    updates

---
+++ src/app/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Editor from '../playground/ckeditor';
+
+const App = () => {
+    return(
+        <div>
+            <Editor />
+        </div>
+    );
+}
+
+export default App;
